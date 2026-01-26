d2c62e0ba7ea8d2e9bc48b553987bece9c761ca1
JoaoVictorg07 <joao.gontijo@pdbomdespacho.com.br>
2026-01-26 14:29:03 -0300

    models

--- myflix/admin.py
+++ myflix/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from myflix.models import user, stream
 
-# Register your models here.
+class users(admin.ModelAdmin):
+    list_display = ('id','nome', 'email', 'cpf', 'data de nascimento', 'celular')
+    lisy_display_links = ('id','nome',)
+    list_per_page = 20
+    search_fields = ('nome',)
+
+admin.site.register(user, users)
+
+class streams(admin.ModelAdmin):
+    list_display = ('id','codigo', 'descricao')
+    lisy_display_links = ('id','codigo',)
+    search_fields = ('codigo',)
+
+admin.site.register(streams, streams)
--- myflix/models.py
+++ myflix/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+'''
+    id 
+    nome
+    email
+    cpf (maximo 11 caracteres)
+    data de nascimento
+    numero de telefone(maximo de 14 caracteres)
+'''
+
+class user(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(blank=False, max_length=30)
+    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DateField()
+    celular = models.CharField(max_length=14)
+
+    def __str__(self):
+        return self.nome
+
+''''
+    id 
+    codigo do stream (maximode 10 caracteres)
+    descrição
+    categoria (filme, série e documentario)
+'''
+
+class stream(models.Model):
+    CATEGORIA = (
+        ('F','Filme'),
+        ('S', 'Série'),
+        ('D', 'Documentario'),
+    )
+    codigo = models.IntegerField(max_length=10)
+    descricao = models.CharField(max_length=100, blank=False,)
+    categoria = models.CharField(max_length=1, choices=CATEGORIA, blank=False, default='F')
+
+    def __str__(self):
+        return self.codigo
